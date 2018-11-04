1fbcf65a7a488cc99145e2549d1e197d4d58486e
sy <tndus0175@gmail.com>
2018-11-05 00:15:17 +0900

    Revert "Components Part 2"
    This reverts commit 9c3b02c771f0fc8dab2ea2e583053a0f07263e30.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    };
-  }
-  
   render() {
     return (
       <div>
@@ -23,7 +13,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Menu/>
       </div>
     );
   }
